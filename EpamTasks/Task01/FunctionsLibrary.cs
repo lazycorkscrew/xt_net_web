a0b55662907aff2893c9f16305091681261d6721
lazycorkscrew <54800143+lazycorkscrew@users.noreply.github.com>
2019-11-20 01:08:23 +0400

    Сделал Task 0.1 с 1-го по 7-е задание

---
+++ EpamTasks/Task01/FunctionsLibrary.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    static class FunctionsLibrary
+    {
+        //Task 1.2
+        public static string Triangle(int n)
+        {
+            string result = string.Empty;
+            for(int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    result = $"{result}*";
+                }
+                result = $"{result}{Environment.NewLine}";
+            }
+
+            return result;
+        }
+
+        //Task 1.3
+        public static string AnotherTriangle(int n, int offset = 0)
+        {
+            string result = string.Empty;
+
+            //Количество звёздочек на текущей строке
+            int stars = 1;
+
+            //Количество пробелов на текущей строке
+            int spaces = n-1;
+            
+            for (int i = 1; i <= n; i++)
+            {
+                for (int o = 0; o < offset; o++)
+                {
+                    result = $"{result} ";
+                }
+
+                for (int j = 0; j < spaces; j++)
+                {
+                    result = $"{result} ";
+                }
+                spaces--;
+
+                for (int k = 0; k < stars; k++)
+                {
+                    result = $"{result}*";
+                }
+                stars = (i * 2)+1;
+                result = $"{result}{Environment.NewLine}";
+            }
+
+            return result;
+        }
+
+        //Task 1.4
+        public static string XmasTree(int n)
+        {
+            string result = string.Empty;
+            for(int i = 1; i <= n; i++)
+            {
+                result = $"{result}{AnotherTriangle(i, n-i)}";
+            }
+
+            return result;
+        }
+
+        //Task 1.5
+        public static int SumOfNumbers(int n = 1000)
+        {
+            int sum = 0;
+            for (int i = 1; i < n; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+
+        //Task 1.6
+        
+        public enum TextStyle
+        {
+            Default = 0,
+            Bold = 1,
+            Italic = 2,
+            Bold_Italic = Bold | Italic,
+            Underline = 4,
+            Bold_Underline = Bold | Underline,
+            Italic_Underline = Italic | Underline,
+            Bold_Italic_Underline = Bold | Italic | Underline
+        }
+
+        //Task 1.7
+        public static string ArrayProcessing()
+        {
+            Random r = new Random();
+            int[] array = new int[r.Next(1, 21)];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = r.Next(100);
+            }
+
+            return $"Сгенерированный массив: {ArrayToString(array)}{Environment.NewLine}" +
+                $"Максимальное: {Max(array)}, Минимальное: {Min(array)}{Environment.NewLine}" +
+                $"Отсортированный массив: {ArrayToString(Sort(array))}";
+        }
+
+        public static string ArrayToString(int [] array)
+        {
+            string result = "{";
+            foreach(int i in array)
+            {
+                result = $"{result}{i}, ";
+            }
+
+            return $"{result}}}";
+        }
+
+        public static int Max(int [] array)
+        {
+            if (array.Length <= 0)
+            {
+                return 0;
+            }
+            int max = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if(array[i] > max)
+                {
+                    max = array[i];
+                }
+            }
+
+            return max;
+        }
+
+        public static int Min(int[] array)
+        {
+            if (array.Length <= 0)
+            {
+                return 0;
+            }
+            int min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i] < min)
+                {
+                    min = array[i];
+                }
+            }
+
+            return min;
+        }
+
+        public static int[] Sort(int[] arrayToSort, bool onlyShow = false)
+        {
+            int[] arrayToWork = (onlyShow ? (int[])arrayToSort.Clone() : arrayToSort);
+            int temp;
+
+            for (int i = 1; i != arrayToWork.Length; i++)
+            {
+                for (int j = 0; j < arrayToWork.Length - i; j++)
+                {
+                    if (arrayToWork[j] > arrayToWork[j + 1])
+                    {
+                        temp = arrayToWork[j + 1];
+                        arrayToWork[j + 1] = arrayToWork[j];
+                        arrayToWork[j] = temp;
+                    }
+                }
+            }
+            return arrayToWork;
+        }
+
+        
+
+
+    }
+}
---
+++ EpamTasks/Task01/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Добрый день. Давайте приступим к заданиям.");
+            Console.ReadKey();
+            Console.WriteLine("Оу, чуть не забыл... сейчас, цвет текста поменяю. Пару секунд.");
+            Thread.Sleep(2000);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nВот так-то лучше :) Итак, первое задание: вычислить площадь прямоугольника.");
+
+            //Задание 1
+            Console.WriteLine("Введите сторону А: ");
+            decimal a = TryReadLineDecimal();
+            Console.WriteLine("Введите сторону B: ");
+            decimal b = TryReadLineDecimal();
+            Console.WriteLine($"Площадь прямоугольника со сторонами {a} и {b} равна {a*b}.");
+            Console.ReadKey();
+
+            //Задание 2
+            Console.WriteLine($"\nЗадание № 2 - прямоугольный треугольник из звёздочек. Сколько строк вы хотите им занять?");
+            Console.WriteLine(FunctionsLibrary.Triangle(TryReadLineInt()));
+            Console.ReadKey();
+
+            //Задание 3
+            Console.WriteLine($"\nЗадание № 3 - Равнобедренный треугольник из звёздочек. Сколько строк вы хотите им занять?");
+            Console.WriteLine(FunctionsLibrary.AnotherTriangle(TryReadLineInt()));
+            Console.ReadKey();
+
+            //Задание 4
+            Console.WriteLine($"\nЗадание № 4 - Ёлочка из звёздочек. Сколько строк вы хотите ей занять?");
+            Console.WriteLine(FunctionsLibrary.XmasTree(TryReadLineInt()));
+            Console.ReadKey();
+
+            //Задание 5
+            Console.WriteLine($"\nЗадание № 5 - Сумма всех чисел, кратных 3 и 5 равна : {FunctionsLibrary.SumOfNumbers()}");
+            Console.WriteLine();
+            Console.ReadKey();
+            
+            //Задание 6
+            ConsoleKeyInfo consoleKey;
+            FunctionsLibrary.TextStyle textStyles = FunctionsLibrary.TextStyle.Default;
+            
+            do
+            {
+                Console.WriteLine($"\nЗадание № 6 - Стили текста.");
+                Console.WriteLine("1 - добавить/убрать Bold\n2 - добавить/убрать Italic\n3 - добавить/убрать Underline\nEnter - следующее задание");
+                consoleKey = Console.ReadKey();
+
+                switch(consoleKey.Key)
+                {
+                    case ConsoleKey.D1 : textStyles ^= FunctionsLibrary.TextStyle.Bold; break;
+                    case ConsoleKey.D2: textStyles ^= FunctionsLibrary.TextStyle.Italic; break;
+                    case ConsoleKey.D3: textStyles ^= FunctionsLibrary.TextStyle.Underline; break;
+                    default: break;
+                }
+
+                Console.Clear();
+                Console.SetCursorPosition(0, 7);
+                Console.Write($"\rТеперь стиль текста будет {textStyles.ToString()}");
+                Console.SetCursorPosition(0, 0);
+            }
+            while (consoleKey.Key != ConsoleKey.Enter);
+            Console.Clear();
+
+            //Задание 7
+            Console.WriteLine($"\nЗадание № 7 - Случайный массив: мин, макс, сортировка, вывод на экран.");
+            Console.WriteLine(FunctionsLibrary.ArrayProcessing());
+            Console.ReadKey();
+
+
+
+            
+        }
+
+        //Функция безопасного ввода целых чисел
+        static int TryReadLineInt()
+        {
+            Console.Write("> ");
+            int n;
+            while (!(int.TryParse(Console.ReadLine(), out n) && n > 0))
+            {
+                Console.WriteLine("Неправильно введено значение.");
+                Console.Write("> ");
+            }
+            return n;
+        }
+
+        //Функция безопасного ввода вещественных чисел
+        static decimal TryReadLineDecimal()
+        {
+            Console.Write("> ");
+            decimal n;
+            while (!(decimal.TryParse(Console.ReadLine(), out n) && n > 0))
+            {
+                Console.WriteLine("Неправильно введено значение.");
+                Console.Write("> ");
+            }
+            return n;
+        }
+    }
+}
